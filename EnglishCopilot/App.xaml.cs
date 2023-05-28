550fc7d582695102a7e49dfaa87d668662fc9ebd
niltor <zpty@outlook.com>
2023-05-28 21:57:07 +0800

    add setting page

--- EnglishCopilot/App.xaml.cs
+++ EnglishCopilot/App.xaml.cs
@@ -7,4 +7,18 @@ public partial class App : Application
         InitializeComponent();
         MainPage = new AppShell();
     }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        // weather current OS is android
+        if (DeviceInfo.Platform == DevicePlatform.WinUI)
+        {
+            window.MaximumWidth = 1080;
+            window.Height = 1080;
+            window.Width = 540;
+        }
+
+        return window;
+    }
 }
--- EnglishCopilot/Pages/SettingsPage.xaml.cs
+++ EnglishCopilot/Pages/SettingsPage.xaml.cs
@@ -6,4 +6,16 @@ public partial class SettingsPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+        AzureKey.Text = Preferences.Get("AzureKey", string.Empty);
+        OpenAIKey.Text = Preferences.Get("OpenAIKey", string.Empty);
+    }
+
+    private void SaveSetting(object sender, EventArgs e)
+    {
+        Preferences.Set("AzureKey", AzureKey.Text);
+        Preferences.Set("OpenAIKey", OpenAIKey.Text);
+    }
 }
